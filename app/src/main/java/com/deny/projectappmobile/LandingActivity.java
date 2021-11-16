5602842ef7b0788e6231e672f02cd8e16f2870e1
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2021-11-16 23:48:19 +0700

    test intent

--- app/src/main/java/com/deny/projectappmobile/LandingActivity.java
+++ app/src/main/java/com/deny/projectappmobile/LandingActivity.java
@@ -2,11 +2,28 @@ package com.deny.projectappmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements View.OnClickListener  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        ImageView btnCreateAcc = findViewById(R.id.create_account);
+        btnCreateAcc.setOnClickListener(this);
+
+        ImageView btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.create_account) {
+            Intent moveCreate = new Intent(LandingActivity.this, MainActivity.class);
+            startActivity(moveCreate);
+        }
     }
 }

2c2817d24c8ab66e4c17cfed21dacbdfeba3df6b
Deny Febriawan <75290185+Deny-lab@users.noreply.github.com>
2021-11-16 23:19:49 +0700

    upload file

---
+++ app/src/main/java/com/deny/projectappmobile/LandingActivity.java
@@ -0,0 +1,12 @@
+package com.deny.projectappmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
